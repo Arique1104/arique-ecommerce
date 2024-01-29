46e13ff8d8138a45f54bb3120b66b92f8c9ae609
Arique1104 <60521756+Arique1104@users.noreply.github.com>
2024-01-29 14:51:52 -0800

    Creates userSnapshot for logging in!

--- src/utils/firebase/firebase.utils.jsx
+++ src/utils/firebase/firebase.utils.jsx
@@ -30,5 +30,23 @@ export const createUserDocumentFromAuth = async (userAuth) => {
     
     const userSnapshot = await getDoc(userDocRef);
     // console.log(userSnapshot);
-    
+    if(!userSnapshot.exists()) {
+        const {displayName, email} = userAuth;
+        const createdAt = new Date();
+       
+        try {
+            await setDoc(userDocRef, {
+                displayName,
+                email,
+                createdAt
+            });
+        } catch (error) {
+            console.log('error creating the user', error.message);
+        }
+    }
+  
+    return userDocRef;  // if user data exists
+    // return userDoc
+    // if user data does not exist
+    // create userDoc
 };
