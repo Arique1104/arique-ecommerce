6e0ce91278c095840bd24ba518db82d4d3792bc0
Arique1104 <60521756+Arique1104@users.noreply.github.com>
2024-01-30 12:11:26 -0800

    Establishes a form that dynamically assigns additional information as provided by the new user login.

--- src/utils/firebase/firebase.utils.jsx
+++ src/utils/firebase/firebase.utils.jsx
@@ -1,5 +1,5 @@
 import { initializeApp } from 'firebase/app';
-import { getAuth, signInWithRedirect, signInWithPopup, GoogleAuthProvider } from 'firebase/auth';
+import { getAuth, signInWithPopup, GoogleAuthProvider, createUserWithEmailAndPassword } from 'firebase/auth';
 import { getFirestore, doc, getDoc, setDoc  } from 'firebase/firestore'
 
 
@@ -24,12 +24,12 @@ export const auth = getAuth();
 export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
 
 export const db = getFirestore();
-export const createUserDocumentFromAuth = async (userAuth) => {
+export const createUserDocumentFromAuth = async (userAuth, additionalInformation = {}
+    ) => {
+    if (!userAuth) return;
     const userDocRef = doc(db, 'users', userAuth.uid);
-    // console.log(userDocRef);
-    
     const userSnapshot = await getDoc(userDocRef);
-    // console.log(userSnapshot);
+    
     if(!userSnapshot.exists()) {
         const {displayName, email} = userAuth;
         const createdAt = new Date();
@@ -38,15 +38,18 @@ export const createUserDocumentFromAuth = async (userAuth) => {
             await setDoc(userDocRef, {
                 displayName,
                 email,
-                createdAt
+                createdAt,
+                ...additionalInformation
             });
         } catch (error) {
             console.log('error creating the user', error.message);
         }
     }
   
-    return userDocRef;  // if user data exists
-    // return userDoc
-    // if user data does not exist
-    // create userDoc
+    return userDocRef; 
+};
+
+export const createAuthUserWithEmailAndPassword = async (email, password) => {
+    if (!email || !password) return;
+    return await createUserWithEmailAndPassword(auth, email, password)
 };
