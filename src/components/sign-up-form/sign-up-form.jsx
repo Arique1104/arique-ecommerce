4ccc4f5c7f334e7695d873c1029c088ba785a058
Arique1104 <60521756+Arique1104@users.noreply.github.com>
2024-01-30 12:12:25 -0800

    Creates a handleSubmit function for the Sign Up form that creates a new user when certain user name parameters are met.

--- src/components/sign-up-form/sign-up-form.jsx
+++ src/components/sign-up-form/sign-up-form.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { createAuthUserWithEmailAndPassword, createUserDocumentFromAuth } from '../../utils/firebase/firebase.utils';
 
 const defaultFormFields = {
     displayName: '',
@@ -10,7 +11,32 @@ const defaultFormFields = {
 const SignUpForm = () => {
     const [formFields, setFormFields] = useState(defaultFormFields);
     const {displayName, email, password, confirmPassword} = formFields;
-console.log(formFields)
+    
+    const resetFormFields = () => {
+        setFormFields(defaultFormFields)
+    }
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        
+        if(password !== confirmPassword ) {
+                alert("passwords do not match");
+            return;
+       }
+       try {
+           const { user } = await createAuthUserWithEmailAndPassword(email, password);
+           await createUserDocumentFromAuth(user, {displayName});
+           resetFormFields();
+            
+      } catch(error){
+            if(error.code === 'auth/email-already-in-use') {
+                alert('Cannot creat user, email already in use')
+            } else {
+                console.log('user creation encountered an error', error)
+            }
+       }
+    };
+    
     const handleChange = (event) => {
         const {name, value} = event.target;
         setFormFields({...formFields, [name]: value})
@@ -19,7 +45,7 @@ console.log(formFields)
     return (
        <div>
         <h1>Sign up with your email and password</h1>
-        <form onSubmit={()=> {}}>
+        <form onSubmit={handleSubmit}>
             <label>Display Name</label>
                 <input 
                     type='text' 
@@ -48,7 +74,7 @@ console.log(formFields)
                     onChange={handleChange} name='confirmPassword'
                     value={confirmPassword} />
 
-            <button type='submit'>Sign Up</button>
+                <button type='submit'>Sign Up</button>
         </form>
        </div> 
     );
